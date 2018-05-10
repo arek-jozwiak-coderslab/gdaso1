b1ee7a79277e305f64008aafb6116dd82f6ee352
arcyro <arek@arcyro.pl>
2018-05-10 16:45:39 +0200

    day 4

---
+++ src/main/java/pl/coderslab/mvc/Mvc132.java
@@ -0,0 +1,40 @@
+package pl.coderslab.mvc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/mvc13")
+public class Mvc132 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String color1 = request.getParameter("color1");
+        String brand1 = request.getParameter("brand1");
+        String model1 = request.getParameter("model1");
+        String color2 = request.getParameter("color2");
+        String brand2 = request.getParameter("brand2");
+        String model2 = request.getParameter("model2");
+
+        Car c1 = new Car(color1, brand1, model1);
+        Car c2 = new Car(color2, brand2, model2);
+
+        List<Car> cars= new ArrayList<>();
+        cars.add(c1);
+        cars.add(c2);
+
+        request.setAttribute("cars", cars);
+
+        getServletContext().getRequestDispatcher("/mvc132result.jsp")
+                .forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/mvc132form.jsp")
+                .forward(request, response);
+    }
+}
---
+++ src/main/webapp/mvc132form.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dell
+  Date: 10.05.18
+  Time: 15:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="color1"/>
+    <input type="text" name="brand1"/>
+    <input type="text" name="model1"/>
+    <input type="text" name="color2"/>
+    <input type="text" name="brand2"/>
+    <input type="text" name="model2"/>
+    <input type="submit"/>
+</form>
+</body>
+</html>
