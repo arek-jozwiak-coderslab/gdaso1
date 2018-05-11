0fb2fe7985c210d133661e570c01c8dbab7a6ba3
arcyro <arek@arcyro.pl>
2018-05-11 15:56:15 +0200

    jdbc

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,31 @@
-CREATE TABLE `books` (
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `isbn` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book`
+--
 ALTER TABLE `book`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
